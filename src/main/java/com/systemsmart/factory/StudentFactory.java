56bd4a6980ee40f1f777c1e374dd20f86cf49699
JunaidMartin <60959473+JunaidMartin@users.noreply.github.com>
2020-07-04 14:38:15 +0200

    Add files via upload
    Made changes to StudentFactory

--- src/main/java/com/systemsmart/factory/StudentFactory.java
+++ src/main/java/com/systemsmart/factory/StudentFactory.java
@@ -3,11 +3,17 @@ package com.systemsmart.factory;
 import com.systemsmart.entity.Student;
 public class StudentFactory {
 
-    public static Student getStudent(long userID, String firstName, String lastName){
-        return (Student) new Student.Builder()
-                .setUserID(userID)
+    public static Student createStudent(long studNum, String firstName, String lastName,
+                                        String campus, long identityNum, String studEmail, String contactNum){
+        Student student = new Student.Builder()
+                .setStudNum(studNum)
                 .setFirstName(firstName)
                 .setLastName(lastName)
+                .setCampus(campus)
+                .setIdentityNum(identityNum)
+                .setStudEmail(studEmail)
+                .setContactNum(contactNum)
                 .build();
+        return student;
     }
 }
