942e972a8ba5394cedb6f9af1f15d58fa939cc36
JunaidMartin <60959473+JunaidMartin@users.noreply.github.com>
2020-07-04 14:36:33 +0200

    Add files via upload
    made changes to Student entity

--- src/main/java/com/systemsmart/entity/Student.java
+++ src/main/java/com/systemsmart/entity/Student.java
@@ -1,29 +1,98 @@
 package com.systemsmart.entity;
 
-public class Student extends UserType{
+public class Student{
+    private long studNum;
+    private String firstName;
+    private String lastName;
+    private String campus;
+    private long identityNum;
+    private String studEmail;
+    private String contactNum;
 
-    public Student(){
-    }
     public Student(Builder builder) {
-        super(builder);
+        this.studNum = builder.studNum;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.campus = builder.campus;
+        this.identityNum = builder.identityNum;
+        this.studEmail = builder.studEmail;
+        this.contactNum = builder.contactNum;
     }
 
-    public static class Builder extends UserType.Builder{
+    // Getters
+    public long getStudNum() {
+        return studNum;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getCampus() {
+        return campus;
+    }
+    public long getIdentityNum() {
+        return identityNum;
+    }
+    public String getStudEmail() {
+        return studEmail;
+    }
+    public String getContactNum() {
+        return contactNum;
+    }
 
-        public Builder() {
-            super();
+    //Builder Class
+    public static class Builder{
+        private long studNum;
+        private String firstName;
+        private String lastName;
+        private String campus;
+        private long identityNum;
+        private String studEmail;
+        private String contactNum;
+
+        public Builder setStudNum (long studNum){
+            this.studNum = studNum;
+            return this;
         }
-
-        public Builder copy(Student student){
-            super.setGuest(student.getGuest());
-            super.setUserID(student.getUserID());
-            super.setFirstName(student.getFirstName());
-            super.setLastName(student.getLastName());
+        public Builder setFirstName (String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+        public Builder setLastName (String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+        public Builder setCampus (String campus){
+            this.campus = campus;
+            return this;
+        }
+        public Builder setIdentityNum (long identityNum){
+            this.identityNum = identityNum;
+            return this;
+        }
+        public Builder setStudEmail (String studEmail){
+            this.studEmail = studEmail;
+            return this;
+        }
+        public Builder setContactNum (String contactNum){
+            this.contactNum = contactNum;
             return this;
         }
 
-        @Override
-        public UserType build(){
+        public Builder copy(Student student){
+            this.studNum = student.studNum;
+            this.firstName = student.firstName;
+            this.lastName = student.lastName;
+            this.campus = student.campus;
+            this.identityNum = student.identityNum;
+            this.studEmail = student.studEmail;
+            this.contactNum = student.contactNum;
+            return this;
+        }
+
+        public Student build(){
             return new Student(this);
         }
 
