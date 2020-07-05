c01ed036d242a58c24047ad9746c205dbeba8dcd
Luzuko Tshaka <luzukotshaka2@gmail.com>
2020-07-05 21:02:42 +0200

    facility factors and Test complted

---
+++ src/main/java/com/systemsmart/factory/FacilityFactory.java
@@ -0,0 +1,28 @@
+package com.systemsmart.factory;
+/*@ Author: Luzuko Tshaka  <216007542@mycput.ac.za>
+  @ description : Domain Driven Design - Facility Factory
+  @ Date: -05/07
+*/
+
+import com.systemsmart.entity.Facility;
+
+public class FacilityFactory {
+
+    public static Facility createFacility(Boolean laundromat, Boolean studyRoom, Boolean disabledFriendly, boolean securityPersonnel,
+                                          boolean computerEquipment, boolean wifiAccess, String electricitySystem){
+
+
+        Facility facility = new Facility.Builder()
+                .setLaundromat(laundromat)
+                .setStudyRoom(studyRoom)
+                .setDisabledFriendly(disabledFriendly)
+                .setSecurityPersonnel(securityPersonnel)
+                .setComputerEquipment(computerEquipment)
+                .setWifiAccess(wifiAccess)
+                .setElectricitySystem(electricitySystem)
+                .buil();
+        return facility;
+
+    }
+
+}
