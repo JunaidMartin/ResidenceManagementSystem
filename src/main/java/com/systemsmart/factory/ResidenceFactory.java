7778ea0f09441d125cb3f08c1f7e8a601b98e8bf
JunaidMartin <60959473+JunaidMartin@users.noreply.github.com>
2020-07-05 18:20:22 +0200

    Update ResidenceFactory.java
    Added class comment.

--- src/main/java/com/systemsmart/factory/ResidenceFactory.java
+++ src/main/java/com/systemsmart/factory/ResidenceFactory.java
@@ -2,6 +2,10 @@ package com.systemsmart.factory;
 
 import com.systemsmart.entity.Residence;
 
+/* Author: Brandon Eugene Charles <218220065@mycput.ac.za>
+ * Description: ResidenceFactory to build Residence entity.
+ * Date: 05/07
+ */
 public class ResidenceFactory {
 
     public static Residence createResidence(int residenceId, int numberOfRooms, String residenceName){
