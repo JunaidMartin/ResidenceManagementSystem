7e6d9f69ee815a20f75a98d1a1620ffe52b82ad2
Mzileni <216279631@mycput.ac.za>
2020-08-30 20:48:52 +0200

    Task completed

--- src/main/java/com/systemsmart/factory/RoomFactory.java
+++ src/main/java/com/systemsmart/factory/RoomFactory.java
@@ -3,7 +3,7 @@ package com.systemsmart.factory;
 import com.systemsmart.entity.Room;
 
 public class RoomFactory {
-    public static Room createRoom(String type, int roomNumber, double price, boolean status) {
+    public static Room createRoom(String type, long roomNumber, double price, boolean status) {
         return new Room.Builder().setRoomNumber(roomNumber)
                 .setType(type).setPrice((int) price)
                 .setStatus(status).build();
