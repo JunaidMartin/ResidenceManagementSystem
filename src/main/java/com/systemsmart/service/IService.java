00b6a3dbd964eab6492022a7e660ffc89c63a641
JunaidMartin <60959473+JunaidMartin@users.noreply.github.com>
2020-09-07 07:50:55 +0200

    Delete IService.java

--- src/main/java/com/systemsmart/service/IService.java
+++
@@ -1,8 +0,0 @@
-package com.systemsmart.service;
-
-public interface IService <entityType, datatypeOfID, R> {
-    entityType create (entityType t);
-    entityType read (datatypeOfID id);
-    entityType update (entityType t);
-    boolean delete (datatypeOfID id);
-}
