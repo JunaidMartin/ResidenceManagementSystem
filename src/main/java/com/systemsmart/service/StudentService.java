6479323fd81a36bd96f88ebf205b6c371520318e
JunaidMartin <60959473+JunaidMartin@users.noreply.github.com>
2020-09-07 09:44:14 +0200

    Update StudentService.java

--- src/main/java/com/systemsmart/service/StudentService.java
+++ src/main/java/com/systemsmart/service/StudentService.java
@@ -4,7 +4,7 @@ import com.systemsmart.entity.Student;
 
 import java.util.Set;
 
-public interface StudentService extends IService <Student, String, A> {
+public interface StudentService extends IService <Student, String> {
     Set<Student> getAll();
     // unique business logic as per student.
     Set<Student> getStudentsWithStudentNumbers216();
