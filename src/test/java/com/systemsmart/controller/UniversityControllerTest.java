958b67da5ab3d07e868582bb76930d6c2ca01b5b
PhumiHoho <phumihoho@gmail.com>
2020-09-26 00:20:53 +0200

    Made changes to entities and did controllers and their tests

---
+++ src/test/java/com/systemsmart/controller/UniversityControllerTest.java
@@ -0,0 +1,78 @@
+package com.systemsmart.controller;
+
+import com.systemsmart.entity.University;
+import com.systemsmart.factory.UniversityFactory;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UniversityControllerTest {
+
+    private static University university = UniversityFactory.createUniversity("CPUT");
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    private String baseURL = "http://localhost:8080/university/";
+
+    @Test
+    public void a_create() {
+        String url = baseURL + "create";
+        System.out.println("URL:" + url);
+        System.out.println("Post date:" + university);
+        ResponseEntity<University> postResponse = restTemplate.postForEntity(url, university, University.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+        university = postResponse.getBody();
+        System.out.println("Saved data:" + university);
+        assertEquals(university.getUniversityName(), postResponse.getBody().getUniversityName());
+    }
+
+    @Test
+    public void d_getall() {
+        String url = baseURL + "all";
+        System.out.println("URL: " + url);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println(response);
+        System.out.println(response.getBody());
+    }
+
+    @Test
+    public void b_read(){
+        String url = baseURL + "read/" + university.getUniversityName();
+        System.out.println("URL: " + url);
+        ResponseEntity<University> response = restTemplate.getForEntity(url, University.class);
+        assertEquals(university.getUniversityName(), response.getBody().getUniversityName());
+    }
+
+    @Test
+    public void c_update(){
+        University updated = new University.Builder().copy(university).setUniversityName("CPUT").build();
+        String url = baseURL + "update";
+        System.out.println("URL: " + url);
+        System.out.println("Post date: " + updated);
+        ResponseEntity<University> response = restTemplate.postForEntity(url, updated, University.class);
+        assertEquals(university.getUniversityName(), response.getBody().getUniversityName());
+    }
+    @Test
+    public void e_delete() {
+        String url = baseURL + "delete/" +university.getUniversityName();
+        System.out.println("URL: " + url);
+        restTemplate.delete(url);
+    }
+}
